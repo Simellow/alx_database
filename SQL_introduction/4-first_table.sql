18c739330c5f8f1b21543507b5439591df509d2e
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-09 16:57:22 +0300

    1

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- A script that creates a table called first_table in the database hbtn_0c_0
+-- A script that creates a table called "first_table" in the database "hbtn_0c_0"
 CREATE TABLE first_table (
     id INT,
     name VARCHAR(256)
