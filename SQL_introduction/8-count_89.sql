3eb5f45d3c33f419d31c195cdb7f4bd87dbd79db
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-09 17:03:57 +0300

    1

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -2,4 +2,8 @@
 -- with id = 89 in the table "first_table" of 
 -- the database "hbtn_0c_0" 
 
-SELECT COUNT(*) FROM first_table WHERE id = '89';
+-- SELECT COUNT(*) FROM first_table WHERE id = '89';
+
+
+SELECT COUNT(id=89) 
+FROM first_table;
