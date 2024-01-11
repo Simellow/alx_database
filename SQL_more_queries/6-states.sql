9eab291cb94737ccd2f07fecc430a13f42f5f1fa
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-11 13:11:50 +0300

    1

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_usa;
 
 -- create a table with specifications
 CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
 
