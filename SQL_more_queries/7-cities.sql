fb2b64e70184d511fef1e4b4993f8566c0ea3e5a
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-11 13:04:27 +0300

    1

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY state_id REFERENCES states(id)
+    FOREIGN KEY state_id REFERENCES hbtn_0d_usa.states(id)
 );
 
 
