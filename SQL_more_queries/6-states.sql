6332c592d12406366aa5d2faff739032df89ef46
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-11 12:47:08 +0300

    1

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,16 +1,19 @@
--- Write a script that creates the database "hbtn_0d_usa" and the table "states"
+-- -- Write a script that creates the database "hbtn_0d_usa" and the table "states"
+-- CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- --using database
+-- USE hbtn_0d_usa;
+
+-- CREATE TABLE IF NOT EXISTS states(
+--     id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(256) NOT NULL
+-- );
+
+
+-- Create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
---using database
 USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
-);
-
-
-
+CREATE TABLE IF NOT EXISTS states(id int UNIQUE PRIMARY KEY AUTO_INCREMENT, name varchar(256) NOT NULL); 
 
 
 
