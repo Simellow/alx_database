ebf1d584b738fdc81aa5851d8e7225fa5d413c09
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-11 13:09:51 +0300

    1

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_usa;
 
 -- creating a table "cities"
 CREATE TABLE IF NOT EXISTS cities(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
